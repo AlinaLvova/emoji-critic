712def99eb57607e16ed54d051680498f908d436
Lvova Alina <lvova.aline@gmail.com>
2023-03-29 17:20:25 +0400

    feature: add nav menu

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Dashboard from './Dashboard';
 import Header from './Header';
 import AboutMe from "./AboutMe";
+import AboutUs from "./AboutUs";
 import Reviews from "./Reviews";
 
 import { Route, Routes } from 'react-router-dom';
@@ -14,7 +15,8 @@ function App() {
       <Routes>
       <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />      </Routes>
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/about-us" element={<AboutUs />} />      </Routes>
     </div>
   );
 }
